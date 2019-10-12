60d508768efc5b0cf5379813363790941178ed5d
Nedal jedidi <nedal.jed@gmail.com>
2019-10-12 23:04:52 +0200

    Sonar

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,10 +16,4 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-
-  public void method(){
-    for (;;) {  // Noncompliant; end condition omitted
-      // ...
-    }
-  }
 }
